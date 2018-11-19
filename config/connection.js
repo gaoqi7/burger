56061771c0eb24d282ecb97c1519413b9750f093
gaoqi7 <gaoqi7@gmail.com>
2018-11-18 20:54:44 -0800

    add heroku support

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,26 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "woaiwojia8",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "woaiwojia8",
+//     database: "burgers_db"
+// });
+var connection;
+if (process.env.JAWSDB_URL)
+{
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
+{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "woaiwojia8",
+        database: "burgers_db"
+
+    });
+}
 connection.connect(err =>
 {
     if (err) throw err;
