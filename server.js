3a6e64bee18572f0466d09d4b7bbb5dd3d032333
gaoqi7 <gaoqi7@gmail.com>
2018-11-20 18:29:04 -0800

    change port 5000

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
-const PORT = 8080 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller");
@@ -16,8 +14,7 @@ var routes = require("./controllers/burgers_controller");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function ()
-{
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
